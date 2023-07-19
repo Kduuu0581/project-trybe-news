c2907a01724b998d7d4a23671a1f4fbd19a64ec9
Carlos Eduardo Oliveira (Kadu) - Turma 24 - Tribo A <carloseduardooliveira81@gmail.com>
2023-07-19 17:35:00 -0300

    4 - Crie a função scrape_news

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,7 @@
 import requests
 import time
 from parsel import Selector
+import re
 
 
 # Requisito 1
@@ -21,22 +22,16 @@ def fetch(url):
 # Requisito 2
 def scrape_updates(html_content):
     selector = Selector(text=html_content)
-    news_cards = selector.css(
-        ".entry-title a::attr(href)"
-        ).getall()
+    news_cards = selector.css(".entry-title a::attr(href)").getall()
     return news_cards
 
 
 # Requisito 3
 def scrape_next_page_link(html_content):
     selector = Selector(text=html_content)
-    next_page = selector.css(
-        ".next.page-numbers"
-    )
+    next_page = selector.css(".next.page-numbers")
     if next_page:
-        next_page_link = next_page.css(
-            "::attr(href)"
-        ).get()
+        next_page_link = next_page.css("::attr(href)").get()
         return next_page_link
     else:
         return None
@@ -44,8 +39,26 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    selector = Selector(text=html_content)
+    url = selector.css('link[rel="canonical"]::attr(href)').get()
+    title = selector.css("h1.entry-title::text").get().strip("\xa0")
+    timestamp = selector.css(".meta-date::text").get()
+    writer = selector.css(".author a::text").get()
+    reading_time = int(
+        selector.css(".meta-reading-time::text").get().split(' ')[0]
+    )
+    summary = selector.css(".entry-content p").get()
+    summary = re.sub("<.*?>", "", summary).strip()
+    category = selector.css(".label::text").get()
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "reading_time": reading_time,
+        "summary": summary,
+        "category": category,
+    }
 
 
 # Requisito 5
